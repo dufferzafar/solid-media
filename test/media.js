d781e2d44e20092692cac5fae480f8ec85af470b
Shadab Zafar <dufferzafar0@gmail.com>
2018-04-15 17:05:28 +0530

    tests: Use async/await instead of promise chains

--- test/media.js
+++ test/media.js
@@ -1,77 +1,41 @@
 var MediaMarket = artifacts.require("./MediaMarket.sol");
 
 contract("MediaMarket", function(accounts) {
-    var MediaInstance;
+    var market;  // Will store an instance of our contract
 
-    it("initializes with two media files", function() {
+    beforeEach('setup contract for each test', async function () {
+        market = await MediaMarket.deployed();
+    })
 
-        return MediaMarket.deployed()
-
-        .then(function(instance) {
-            return instance.media_count();
-        })
-
-        .then(function(count) {
-            assert.equal(count, 2);
-        });
-
+    it("initializes with one media file", async function() {
+        assert.equal(await market.media_count(), 1);
     });
 
-    it("initializes the media files with \'correct\' values", function() {
-
-        return MediaMarket.deployed()
-
-        .then(function(instance) {
-            MediaInstance = instance;
-            return MediaInstance.media_store(1);
-        })
-
-        .then(function(media) {
-            assert.equal(media[0], 1, "contains the correct id");
-            assert.equal(media[1], "If I lose myself", "contains the correct name");
-            return MediaInstance.media_store(2);
-        })
-
-        .then(function(media) {
-            assert.equal(media[0], 2, "contains the correct id");
-            assert.equal(media[1], "Avengers: Infinity War", "contains the correct name");
-        });
+    it("initializes the media file with \'correct\' values", async function() {
+        media = await market.media_store(1);
+        assert.equal(media[0], 1);
+        assert.equal(media[1], "If I lose myself");
     });
 
-    // TODO: More tests here
+    // it("adds a media entry", async function() {});
 
-    // it("adds a media entry", function() {});
+    it("lists available media", async function() {
+        observed_list = [];
+        expected_list = ["If I lose myself", "Avengers: Infinity War"];
 
-    it("lists available media", function() {
+        media_count = await market.media_count();
+        for (let i = 1; i <= media_count; i++) {
 
-        return MediaMarket.deployed().then(function(instance) {
-            MediaInstance = instance;
-            return MediaInstance.media_count();
-        })
+            media = await market.media_store(i)
+            observed_list.push(media[1]);
 
-        .then(function(media_count) {
-
-            // Store names of all available media
-            // So that we can list compare
-            var media_list = [];
-            var expected_list = ["If I lose myself", "Avengers: Infinity War"];
-
-            // Build media list one by one
-            // Promise chain is needed because it is all async!
-            for (var i = 1; i <= media_count; i++) {
-                MediaInstance.media_store(i).then(function(media) {
-                    var name = media[1];
-                    media_list.push(name);
-
-                    // TODO: Find a way to do this outside the loop
-                    // Just moving this down won't work
-                    // Because the list gets created after some time
-                    // and is still empty before the code reaches there
-                    if (i == media_count) {
-                        assert.deepEqual(media_list, expected_list);
-                    }
-                });
+            // TODO: Find a way to do this outside the loop
+            // Just moving this down won't work
+            // Because the list gets created after some time
+            // and is still empty before the code reaches there
+            if (i == media_count) {
+                assert.deepEqual(observed_list, expected_list);
             }
-        })
+        }
     });
 });
